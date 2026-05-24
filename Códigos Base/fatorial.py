95117aaec201c5468937efb551cd377e5c5e4bd4
Julio-dos-programas17 <120344003+Julio-dos-programas17@users.noreply.github.com>
2026-05-24 17:17:15 -0300

    mudança no cod fatorial.py e adição do arquivo novo

--- Códigos Base/fatorial.py
+++ Códigos Base/fatorial.py
@@ -1,11 +1,7 @@
 def fact(num):
     if num == 2:
         return 2
-    
-    print(f"Antes: {num}")
-    a =  num*fact(num-1)
-    print(f"Depois: {num}")
 
-    return a
+    return num*fact(num-1)
 
-print(fact(5))
+print(fact(4))
